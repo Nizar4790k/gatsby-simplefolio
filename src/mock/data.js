ae6fb647d108991c5c0093a164fe03fd98bd2342
Nizar Sanchez <nizar4790k@gmail.com>
2021-01-15 11:05:41 -0400

    added github and telegram links

--- src/mock/data.js
+++ src/mock/data.js
@@ -64,24 +64,14 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'github',
       url: 'https://github.com/Nizar4790k',
     },
+    {
+      id:nanoid(),
+      name:'telegram',
+      url:'https://t.me/Nizar4790k',
+    }
   ],
 };
 
